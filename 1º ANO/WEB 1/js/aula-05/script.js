552a66839e84d06c88afb8a8ff9ccbc20d676e7b
NatanCancian <natancancian@gmail.com>
2025-10-29 22:29:10 -0300

    teste ajuste

--- 1º ANO/WEB 1/js/aula-05/script.js
+++ 1º ANO/WEB 1/js/aula-05/script.js
@@ -1,6 +1,12 @@
 const tarefas = [];
 const descricaoTarefa = document.getElementById("descricao");
 const statusTarefa = document.getElementById("status");
+<<<<<<< HEAD
 async function criarTarefa() {
+=======
+const dataTarefa = document.getElementById("dataConclusao");
+
+async function adicionarTarefa() {
+>>>>>>> 9360ff2 (Implementação POST pt.1)
     
 } 
