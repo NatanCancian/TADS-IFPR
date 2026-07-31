e8c84e0e3bece28026d48a1369fa3eb871aa05e4
NatanCancian <natancancian@gmail.com>
2026-07-30 22:27:48 -0300

    aula-02

---
+++ 2º ANO/POO/3º BIMESTRE/AULA-02/appEstoque/frontend/src/context/AuhtContext.tsx
@@ -0,0 +1,46 @@
+import { Children, createContext, useContext, useState, type ReactNode } from 'react';
+import type { Usuario } from '../types';
+import { authApi} from '../api/api';
+
+interface AuthContextType{
+    usuario: Usuario | null;
+    logado: boolean;
+    login: (email: string, senha: string) => Promise<void>
+    logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+    const [usuario, setUsuario] = useState<Usuario | null>(() =>{
+        const salvo = localStorage.getItem('usuario')
+        return salvo ? JSON.parse(salvo) : null; 
+    });
+
+async function login(email: string, senha: string){
+    const resposta = await authApi.login(email, senha);
+    localStorage.setItem('accessToken', resposta.accessToken);
+    localStorage.setItem('usuario', JSON.stringify(resposta.usuario));
+    setUsuario(resposta.usuario);
+}
+
+function logout(){
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('usuario');
+    setUsuario(null);
+}
+
+return (
+    <AuthContext.Provider value={{usuario, logado: !!usuario, login, logout}}>
+        {children}
+    </AuthContext.Provider>
+);
+}
+
+export function useAuth(){
+    const context = useContext(AuthContext);
+    if(!context){
+        throw new Error ('useAuth deve ser usado dentro de um AuthProvider';)
+    }
+    return context;
+}
